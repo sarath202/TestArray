f1ccc682b5f7478f27ad9e9eea5846adb92c1fc0
sarath202 <79498227+sarath202@users.noreply.github.com>
2021-06-04 10:57:53 +0530

    Java Matrix Multiplication

---
+++ TestArrays.java
@@ -0,0 +1,25 @@
+public class TestArray {
+
+    public static void main(String[] args) {
+        int rows = 3, columns = 3;
+        int[][] firstMatrix = {{1,3,4},{2,4,3},{3,4,5}};
+        int[][] secondMatrix = {{1,3,4},{2,4,3},{1,2,4}};
+
+        // Adding Two matrices
+        int[][] sum = new int[rows][columns];
+        for(int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                sum[i][j] = firstMatrix[i][j] * secondMatrix[i][j];
+            }
+        }
+
+        // Displaying the result
+        System.out.println("Sum of two matrices is: ");
+        for(int[] row : sum) {
+            for (int column : row) {
+                System.out.print(column + "    ");
+            }
+            System.out.println();
+        }
+    }
+}
